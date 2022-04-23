9823085d589ec5833b21c33d462cceaeb0b50a59
EC2 Default User <ec2-user@ip-172-31-6-164.ec2.internal>
2022-04-23 05:28:11 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,8 @@ class BooksController < ApplicationController
 
         
         else
-          render :new
+          @books = Book.all
+          render :index
         end  
        
     end
@@ -27,6 +28,8 @@ class BooksController < ApplicationController
     def index
         
         @books = Book.all
+        @book = Book.new
+        
         
     end
     
@@ -40,14 +43,21 @@ class BooksController < ApplicationController
     
     def edit
         @book = Book.find(params[:id])
+        
     end
     
     
     def update
-        book = Book.find(params[:id])
-        book.update(book_params)
-         
-        redirect_to book_path(book.id), success: 'successdly'
+        @book = Book.find(params[:id])
+       if  @book.update(book_params)
+       
+        redirect_to book_path, flash: {success: "登録が完了しました"}
+        
+        else
+          @books = Book.all
+          render :edit
+        end  
+        
     end
     
     def destroy
